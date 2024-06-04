d712eee3cfb5df9e9176d39fcf807746a7c8115a
Sk Taslimuddin <sktaslimuddin9@gmail.com>
2024-06-04 22:22:04 +0530

    SpeedInsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -44,6 +45,7 @@ export default function RootLayout({
           
           {children}
           <Toaster />
+          <SpeedInsights />
         </body>
       </ClerkProvider>
       </html>
